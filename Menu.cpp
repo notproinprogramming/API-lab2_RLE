ddcc63db9b0fd1abae3506b6efe785c311f442bc
notproinprogramming <nikarsh7@gmail.com>
2026-02-28 14:07:28 +0200

    remake of previous work with Base64 for easier developing of RLE

---
+++ Menu.cpp
@@ -0,0 +1,53 @@
+#include "Base64.hpp"
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+
+    initDecodeTable();
+
+    while (true) {
+        std::cout << "\n=== MENU ===\n";
+        std::cout << "1) Base64 encode file\n";
+        std::cout << "2) Base64 decode file\n";
+        std::cout << "9) Open .base64 file in nvim\n";
+        std::cout << "0) Exit\n";
+
+        int choose;
+        std::cin >> choose;
+
+        if (choose == 0)
+            break;
+
+        if (choose == 1) {
+            std::string in, out;
+            std::cout << "Input file: ";
+            std::cin >> in;
+            std::cin.ignore();
+            std::cout << "Output file (empty = default): ";
+            std::getline(std::cin, out);
+
+            Base64EncodeFile(in, out);
+        }
+        else if (choose == 2) {
+            std::string in, out;
+            std::cout << "Encoded file: ";
+            std::cin >> in;
+            std::cin.ignore();
+            std::cout << "Output file (empty = default): ";
+            std::getline(std::cin, out);
+
+            Base64DecodeFile(in, out);
+        }
+        else if (choose == 9) {
+            std::string file;
+            std::cout << "File to open: ";
+            std::cin >> file;
+
+            std::string command = "nvim " + file;
+            system(command.c_str());
+        }
+    }
+
+    return 0;
+}
